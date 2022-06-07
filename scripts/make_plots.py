5ecf8326f273547790f6d7c389d9b15927424025
Michael Palumbo <palumbomi95@gmail.com>
2022-06-07 08:03:13 -0400

    init plot

---
+++ scripts/make_plots.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import os, sys, pdb, glob
+
+# use style
+plt.style.use("my.mplstyle"); plt.ioff()
