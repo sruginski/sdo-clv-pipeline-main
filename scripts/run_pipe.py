6c3b892840fac4fdc28decfc1ae363b45a464f17
Michael Palumbo <palumbomi95@gmail.com>
2022-06-08 13:53:19 -0400

    file sorting by datetime

--- scripts/run_pipe.py
+++ scripts/run_pipe.py
@@ -1,8 +1,7 @@
 import numpy as np
+import datetime as dt
 import matplotlib.pyplot as plt
-import pdb
-import csv
-import glob
+import re, pdb, csv, glob
 from astropy.time import Time
 from os.path import exists, split, isdir
 
@@ -12,43 +11,6 @@ plt.style.use("my.mplstyle"); plt.ioff()
 # bring functions into scope
 from sdo_pypline.sdo_image import *
 from sdo_pypline.sdo_vels import *
-
-# function to glob the input data
-def find_sdo_data(indir):
-    # find the data
-    con_files = glob.glob(indir + "*hmi.ic*.fits")
-    mag_files = glob.glob(indir + "*hmi.m*.fits")
-    dop_files = glob.glob(indir + "*hmi.v*.fits")
-    aia_files = glob.glob(indir + "*aia*.fits")
-    return con_files, mag_files, dop_files, aia_files
-
-def write_vels(fname, mjd, vels):
-    # create the file if it doesn't exist
-    if (~exists(fname) & isdir(split(fname)[0])):
-        with open(fname, "w") as f:
-            writer = csv.writer(f)
-            writer.writerow(["mjd", "v_hat", "v_phot", "v_quiet", "v_conv"])
-
-    # write the vels
-    with open(fname, "a") as f:
-        writer = csv.writer(f)
-        writer.writerow(np.concatenate(([mjd], [v for v in vels])))
-
-    return None
-
-def write_vels_by_region(fname, mjd, region, lo_mu, hi_mu, vels):
-    # create the file if it doesn't exist
-    if (~exists(fname) & isdir(split(fname)[0])):
-        with open(fname, "w") as f:
-            writer = csv.writer(f)
-            writer.writerow(["mjd", "region", "lo_mu", "hi_mu", "v_hat", "v_phot", "v_quiet", "v_conv"])
-
-    # write the vels
-    with open(fname, "a") as f:
-        writer = csv.writer(f)
-        writer.writerow(np.concatenate(([mjd], [region], [lo_mu], [hi_mu], [v for v in vels])))
-
-    return None
 
 # actually do things
 def main():
@@ -60,15 +22,13 @@ def main():
         outdir = "/storage/home/mlp95/work/sdo_output/"
 
     # find the input data
-    con_files, mag_files, dop_files, aia_files = find_sdo_data(indir)
+    con_files, mag_files, dop_files, aia_files = find_data(indir)
 
     # check the lengths
     assert (len(con_files) == len(mag_files) == len(dop_files) == len(aia_files))
 
-    # TODO sort the files by date
-
     # set mu threshold and number of mu_rings
-    n_rings = 12
+    n_rings = 10
     mu_thresh = 0.1
 
     # grid of mu values to iterate over
--- sdo_pypline/sdo_io.py
+++ sdo_pypline/sdo_io.py
@@ -1,6 +1,10 @@
 # necessary modules
+import numpy as np
 import sunpy as sp
+import datetime as dt
+import re, pdb, csv, glob
 from astropy.io import fits
+from os.path import exists, split, isdir
 
 # read headers and data
 def read_header(file):
@@ -9,3 +13,81 @@ def read_header(file):
 def read_data(file):
     return fits.getdata(file, 1).astype(float)
 
+# function to glob the input data
+def find_data(indir):
+    # find the data
+    con_files = sort_data(glob.glob(indir + "*hmi_ic*.fits"))
+    mag_files = sort_data(glob.glob(indir + "*hmi_m*.fits"))
+    dop_files = sort_data(glob.glob(indir + "*hmi_v*.fits"))
+    aia_files = sort_data(glob.glob(indir + "*aia*.fits"))
+    return con_files, mag_files, dop_files, aia_files
+
+def get_dates(files):
+    date = []
+    for f in files:
+        # get date string from regex
+        s = re.search(r'\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}', f)
+        if s is None:
+            s = re.search(r'\d{4}_\d{2}_\d{2}t\d{2}_\d{2}_\d{2}', f)
+
+        # replace any t's with underscore
+        s = s.group()
+        s = s.replace("t", "_")
+
+        # make it a datetime
+        date.append(dt.datetime.strptime(s, "%Y_%m_%d_%H_%M_%S"))
+
+    return date
+
+def sort_data(*f_lists):
+    # get f_lists as list
+    f_lists = list(f_lists)
+
+    # loop over lists of files, sorting
+    for idx, files in enumerate(f_lists):
+        # get the dates
+        dates = get_dates(files)
+
+        # sort them in place
+        inds = np.argsort(dates)
+        files = [files[i] for i in inds]
+        print(idx)
+        f_lists[idx] = files
+
+    return tuple(f_lists)
+
+def write_vels(fname, mjd, vels):
+    # truncate the file if it does exist
+    if exists(fname):
+        with open(fname, "w") as f:
+            f.truncate()
+    # create the file if it doesn't exist
+    elif (~exists(fname) & isdir(split(fname)[0])):
+        with open(fname, "w") as f:
+            writer = csv.writer(f)
+            writer.writerow(["mjd", "v_hat", "v_phot", "v_quiet", "v_conv"])
+
+    # write the vels
+    with open(fname, "a") as f:
+        writer = csv.writer(f)
+        writer.writerow(np.concatenate(([mjd], [v for v in vels])))
+
+    return None
+
+def write_vels_by_region(fname, mjd, region, lo_mu, hi_mu, vels):
+    # truncate the file if it does exist
+    if exists(fname):
+        with open(fname, "w") as f:
+            f.truncate()
+    # create the file if it doesn't exist
+    if (~exists(fname) & isdir(split(fname)[0])):
+        with open(fname, "w") as f:
+            writer = csv.writer(f)
+            writer.writerow(["mjd", "region", "lo_mu", "hi_mu", "v_hat", "v_phot", "v_quiet", "v_conv"])
+
+    # write the vels
+    with open(fname, "a") as f:
+        writer = csv.writer(f)
+        writer.writerow(np.concatenate(([mjd], [region], [lo_mu], [hi_mu], [v for v in vels])))
+
+    return None
