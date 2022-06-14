da3a79187159205454810d9733bb57f825f6a2f6
Michael Palumbo <palumbomi95@gmail.com>
2022-06-14 10:13:07 -0400

    epoch matching

--- sdo_pypline/sdo_io.py
+++ sdo_pypline/sdo_io.py
@@ -16,11 +16,26 @@ def read_data(file):
 # function to glob the input data
 def find_data(indir):
     # find the data
-    con_files = sort_data(glob.glob(indir + "*hmi_ic*.fits"))
-    mag_files = sort_data(glob.glob(indir + "*hmi_m*.fits"))
-    dop_files = sort_data(glob.glob(indir + "*hmi_v*.fits"))
-    aia_files = sort_data(glob.glob(indir + "*aia*.fits"))
+    con_files, con_dates = sort_data(glob.glob(indir + "*hmi_ic*.fits"))
+    mag_files, mag_dates = sort_data(glob.glob(indir + "*hmi_m*.fits"))
+    dop_files, dop_dates = sort_data(glob.glob(indir + "*hmi_v*.fits"))
+    aia_files, aia_dates = sort_data(glob.glob(indir + "*aia*.fits"))
+
+    # find datetimes that are in *all* lists
+    common_dates = list(set.intersection(*map(set, [con_dates, mag_dates, dop_dates, aia_dates])))
+
+    # remove epochs that are missing in any data set from all data sets
+    con_files = [con_files[idx] for idx, date in enumerate(con_dates) if date in common_dates]
+    mag_files = [mag_files[idx] for idx, date in enumerate(mag_dates) if date in common_dates]
+    dop_files = [dop_files[idx] for idx, date in enumerate(dop_dates) if date in common_dates]
+    aia_files = [aia_files[idx] for idx, date in enumerate(aia_dates) if date in common_dates]
     return con_files, mag_files, dop_files, aia_files
+
+def sort_data(f_list):
+    # get the dates and indices to sort
+    dates = get_dates(f_list)
+    inds = np.argsort(dates)
+    return [f_list[i] for i in inds], [dates[i] for i in inds]
 
 def get_date(f):
     s = re.search(r'\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}', f)
@@ -30,7 +45,7 @@ def get_date(f):
     # replace any t's with underscore
     s = s.group()
     s = s.replace("t", "_")
-    return dt.datetime.strptime(s, "%Y_%m_%d_%H_%M_%S")
+    return round_time(date=dt.datetime.strptime(s, "%Y_%m_%d_%H_%M_%S"))
 
 def get_dates(files):
     date = []
@@ -40,11 +55,16 @@ def get_dates(files):
 
     return date
 
-def sort_data(f_list):
-    # get the dates and indices to sort
-    dates = get_dates(f_list)
-    inds = np.argsort(dates)
-    return [f_list[i] for i in inds]
+def round_time(date=None, round_to=3600):
+   """Round a datetime object to any time lapse in seconds
+   dt : datetime.datetime object, default now.
+   round_to : Closest number of seconds to round to, default 1 hour.
+   Author: Thierry Husson 2012 - Use it as you want but don't blame me.
+   """
+   if date == None : date = datetime.datetime.now()
+   seconds = (date.replace(tzinfo=None) - date.min).seconds
+   rounding = (seconds+round_to/2) // round_to * round_to
+   return date + dt.timedelta(0,rounding-seconds,-date.microsecond)
 
 def truncate_file(fname):
     # truncate the file if it does exist
