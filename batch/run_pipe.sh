fa3e744391761d3da20b3d278ed5bd554ce5d2f5
Michael Palumbo <palumbomi95@gmail.com>
2022-09-19 10:20:00 -0400

    stuff for file continuing?

--- batch/run_pipe.sh
+++ batch/run_pipe.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=10
 #SBATCH --mem-per-cpu=8192
-#SBATCH --time=36:00:00
+#SBATCH --time=48:00:00
 #SBATCH --job-name=sdo_pipe
 #SBATCH --chdir=/storage/home/mlp95/work/sdo-pypline
 #SBATCH --output=/storage/home/mlp95/work/logs/sdo_pipe.%j.out
@@ -22,7 +22,8 @@ conda activate solar
 echo "Environment activated"
 
 echo "About to start Python"
-python /storage/home/mlp95/work/sdo-pypline/scripts/run_pipe.py --clobber --globexp "*2013*"
+python /storage/home/mlp95/work/sdo-pypline/scripts/run_pipe.py --clobber --globexp "*2012*"
+python /storage/home/mlp95/work/sdo-pypline/scripts/run_pipe.py --clobber "*2013*"
 python /storage/home/mlp95/work/sdo-pypline/scripts/run_pipe.py --globexp "*2014*"
 python /storage/home/mlp95/work/sdo-pypline/scripts/run_pipe.py --globexp "*2015*"
 echo "Python exited"
