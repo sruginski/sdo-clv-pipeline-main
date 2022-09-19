24630aa142e3882a3695d7ad0039487b08339d4e
Michael Palumbo <palumbomi95@gmail.com>
2022-09-19 10:32:10 -0400

    more power

--- batch/download_data.sh
+++ batch/download_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -A ebf11_c
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
+#SBATCH --ntasks=12
 #SBATCH --mem-per-cpu=2048
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --job-name=sdo_data
 #SBATCH --chdir=/storage/home/mlp95/work/sdo-pypline
 #SBATCH --output=/storage/home/mlp95/work/logs/sdo_data.%j.out
@@ -31,6 +31,9 @@ export PJOBLOG="/storage/home/mlp95/work/logs/$SLURM_JOB_NAME.$SLURM_JOBID.pjobl
 parallel="parallel --max-procs $SLURM_NTASKS --joblog $PJOBLOG"
 
 echo "About to start Python w/ gnuparallel"
+$parallel --colsep ',' "$srun python sdo_pypline/sdo_download.py --outdir {1} --start {2} --end {3} --sample {4}" :::: batch/dates2012.txt
+$parallel --colsep ',' "$srun python sdo_pypline/sdo_download.py --outdir {1} --start {2} --end {3} --sample {4}" :::: batch/dates2013.txt
+$parallel --colsep ',' "$srun python sdo_pypline/sdo_download.py --outdir {1} --start {2} --end {3} --sample {4}" :::: batch/dates2014.txt
 $parallel --colsep ',' "$srun python sdo_pypline/sdo_download.py --outdir {1} --start {2} --end {3} --sample {4}" :::: batch/dates2015.txt
 echo "Python exited"
 date
