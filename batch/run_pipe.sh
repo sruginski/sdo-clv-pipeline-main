5e91e4491d5f6bbd08133305e9deb4906a84cb42
Michael Palumbo <palumbomi95@gmail.com>
2022-12-06 15:07:12 -0500

    submit multiple jobs:

--- batch/run_pipe.sh
+++ batch/run_pipe.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
-#SBATCH --account=ebf11_c
-##SBATCH --partition=burst
-##SBATCH --qos=burst2x
-#SBATCH --nodes=1
-#SBATCH --ntasks=16
-#SBATCH --mem-per-cpu=8192
-#SBATCH --time=48:00:00
-#SBATCH --job-name=sdo_pipe
-#SBATCH --chdir=/storage/home/mlp95/work/sdo-pypline
-#SBATCH --output=/storage/home/mlp95/work/logs/sdo_pipe.%j.out
 
-echo "About to start: $SLURM_JOB_NAME"
-date
-echo "Job id: $SLURM_JOBID"
-echo "About to change into $SLURM_SUBMIT_DIR"
-cd $SLURM_SUBMIT_DIR
+one=$(qsub /storage/home/mlp95/work/sdo-pyplinebatch/run2012.sh)
+echo $one
 
-echo "About to activate conda environment"
-source /storage/group/ebf11/default/software/anaconda3/bin/activate
-conda activate solar
-echo "Environment activated"
+two=$(qsub /storage/home/mlp95/work/sdo-pyplinebatch/run2013.sh)
+echo $two
 
-echo "About to start Python"
-python /storage/home/mlp95/work/sdo-pypline/scripts/run_pipe.py --globexp "*2012*"
-python /storage/home/mlp95/work/sdo-pypline/scripts/run_pipe.py --globexp "*2013*"
-python /storage/home/mlp95/work/sdo-pypline/scripts/run_pipe.py --globexp "*2014*"
-python /storage/home/mlp95/work/sdo-pypline/scripts/run_pipe.py --globexp "*2015*"
-echo "Python exited"
-date
+three=$(qsub /storage/home/mlp95/work/sdo-pyplinebatch/run2014.sh)
+echo $three
+
+four=$(qsub /storage/home/mlp95/work/sdo-pyplinebatch/run2015.sh)
+echo $four
