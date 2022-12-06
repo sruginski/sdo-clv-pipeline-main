f126437f3388def8ff0348a47003d533d5a72566
Michael Palumbo <palumbomi95@gmail.com>
2022-12-06 15:28:11 -0500

    fix stitiching

--- batch/run_pipe.sh
+++ batch/run_pipe.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-one=$(qsub /storage/home/mlp95/work/sdo-pyplinebatch/run2012.sh)
+one=$(sbatch /storage/home/mlp95/work/sdo-pyplinebatch/run2012.sh)
 echo $one
 
-two=$(qsub /storage/home/mlp95/work/sdo-pyplinebatch/run2013.sh)
+two=$(sbatch /storage/home/mlp95/work/sdo-pyplinebatch/run2013.sh)
 echo $two
 
-three=$(qsub /storage/home/mlp95/work/sdo-pyplinebatch/run2014.sh)
+three=$(sbatch /storage/home/mlp95/work/sdo-pyplinebatch/run2014.sh)
 echo $three
 
-four=$(qsub /storage/home/mlp95/work/sdo-pyplinebatch/run2015.sh)
+four=$(sbatch /storage/home/mlp95/work/sdo-pyplinebatch/run2015.sh)
 echo $four
