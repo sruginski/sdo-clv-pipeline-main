0ee835225859e524a4b412be275939ed0116eca4
Michael Palumbo <palumbomi95@gmail.com>
2023-01-30 14:20:44 -0500

    job deps

--- batch/run_pipe.sh
+++ batch/run_pipe.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-
 # queue the analysis jobs
-jid1=$(sbatch /storage/home/mlp95/work/sdo-pypline/batch/run2012.sh)
-jid2=$(sbatch /storage/home/mlp95/work/sdo-pypline/batch/run2013.sh)
-jid3=$(sbatch /storage/home/mlp95/work/sdo-pypline/batch/run2014.sh)
-jid4=$(sbatch /storage/home/mlp95/work/sdo-pypline/batch/run2015.sh)
+jid1=$(sbatch --parsable /storage/home/mlp95/work/sdo-pypline/batch/run2012.sh)
+jid2=$(sbatch --parsable /storage/home/mlp95/work/sdo-pypline/batch/run2013.sh)
+jid3=$(sbatch --parsable /storage/home/mlp95/work/sdo-pypline/batch/run2014.sh)
+jid4=$(sbatch --parsable /storage/home/mlp95/work/sdo-pypline/batch/run2015.sh)
 
 # merge the output
-jid5=$(sbatch --dependency=afterok:jid1:jid2:jid3:jid4 /storage/home/mlp95/work/sdo-pypline/batch/merge_output.sh)
-jid6=$(sbatch --dependency=afterok:jid1:jid2:jid3:jid4 /storage/home/mlp95/work/sdo-pypline/batch/preprocess_output.sh)
+jid5=$(sbatch --parsable --dependency=afterok:${jid1}:${jid2}:${jid3}:${jid4} /storage/home/mlp95/work/sdo-pypline/batch/merge_output.sh)
+jid6=$(sbatch --parsable --dependency=afterok:${jid5} /storage/home/mlp95/work/sdo-pypline/batch/preprocess_output.sh)
