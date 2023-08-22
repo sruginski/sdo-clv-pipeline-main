0c233daf0b00dd06b08386b54ea61094187108c8
Michael Palumbo <palumbomi95@gmail.com>
2023-08-21 22:27:54 -0400

    fixed compat issues

--- setup.py
+++ setup.py
@@ -12,12 +12,11 @@ setuptools.setup(
     install_requires=[
         'numpy',
         'pandas',
-        'matplotlib==3.5.1',
-        'PyQt5==5.15.6',
-        'sunpy==4.0.4',
-        'astropy==5.1',
+        'matplotlib==3.5.3',
+        'sunpy==4.0.5',
+        'astropy==5.2',
         'scipy==1.9.2',
-        'reproject==0.8',
+        'reproject==0.9',
         'scikit-image==0.19.3',
         'pyshtools==4.10.3'
     ],
@@ -27,6 +26,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='==3.9.*',
 )
 
