489bcd2eafbd21ec78d5801442913715cdf568cb
Michael Palumbo <palumbomi95@gmail.com>
2025-06-10 14:26:46 -0400

    cleanup

--- sdo_clv_pipeline/legendre.py
+++ sdo_clv_pipeline/legendre.py
@@ -1,35 +1,12 @@
-# code copied/modified from https://github.com/samarth-kashyap/hmi-clean-ls
+# code modified from https://github.com/samarth-kashyap/hmi-clean-ls
 # original author: Samarth Ganesh Kashyap (g.samarth@tifr.res.in)
 # original associated publication: https://arxiv.org/abs/2105.12055
 
 import numpy as np
 from math import pi
-from pyshtools import legendre as pleg
-from scipy.special import lpmv, lpmn, factorial, eval_legendre
-
-def get_pleg_index(l, m):
-    return int(l*(l+1)/2 + m)
+from scipy.special import eval_legendre
 
 
-def gen_leg(lmax, theta):
-    cost = np.cos(theta)
-    sint = np.sin(theta).reshape(1, theta.shape[0])
-
-    maxIndex = int(lmax+1)
-    ell = np.arange(maxIndex)
-    norm = np.sqrt(ell*(ell+1)).reshape(maxIndex, 1)
-    norm[norm == 0] = 1
-
-    leg = np.zeros((maxIndex, theta.size))
-    leg_d1 = np.zeros((maxIndex, theta.size))
-
-    for i,z in enumerate(cost):
-        leg[:, i], leg_d1[:, i] = pleg.PlBar_d1(lmax, z)
-    return leg/np.sqrt(2)/norm, leg_d1 * (-sint)/np.sqrt(2)/norm
-
-"""
-Vectorized version of gen_leg()^
-"""
 def gen_leg_vec(lmax, theta):
     # build cos, sin and l arrays
     theta = theta.value * np.pi / 180.0
@@ -63,22 +40,6 @@ def gen_leg_vec(lmax, theta):
     return leg_out, leg_d1_out
 
 
-def gen_leg_x(lmax, x):
-    maxIndex = int(lmax+1)
-    ell = np.arange(maxIndex)
-    norm = np.sqrt(ell*(ell+1)).reshape(maxIndex, 1)
-    norm[norm == 0] = 1
-
-    leg = np.zeros((maxIndex, x.size))
-    leg_d1 = np.zeros((maxIndex, x.size))
-
-    for i,z in enumerate(x):
-        leg[:, i], leg_d1[:, i] = pleg.PlBar_d1(lmax, z)
-    return leg/np.sqrt(2)/norm, leg_d1/np.sqrt(2)/norm
-
-"""
-Vectorized version of gen_leg_x()^
-"""
 def gen_leg_x_vec(lmax, x):
     # switch to radians
     x = x.value * np.pi / 180.0
@@ -110,6 +71,7 @@ def gen_leg_x_vec(lmax, x):
     leg_out    = leg    / np.sqrt(2) / norm_l[:,None]
     leg_d1_out = leg_dx / np.sqrt(2) / norm_l[:,None]
     return leg_out, leg_d1_out
+
 
 def inv_SVD(A, svdlim):
     u, s, v = np.linalg.svd(A, full_matrices=False)
