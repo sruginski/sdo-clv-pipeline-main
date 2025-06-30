cd704b3f992a19b4cd1fe4fc1f18fecae4043dba
sruginski <ruginski_savannah@wheatoncollege.edu>
2025-06-30 10:30:39 -0400

    rearranging code to loop through data
    Former-commit-id: 9f395efe21eeca40bba7ac9401b1279e7f1d09c6

--- sdo_clv_pipeline/download_stuff.py
+++ sdo_clv_pipeline/download_stuff.py
@@ -1,3 +1,4 @@
 from sdo_clv_pipeline.sdo_download import *
 
-download_data(series="720", email="mlp95@psu.edu", outdir="/directory", start="2014/01/01", end="2014/01/02")
+download_data(series="720", email="mlp95@psu.edu", outdir="/Users/srugins/sdo-clv-pipeline/data", start="2014/01/01", end="2014/01/02", 
+              sample=4, overwrite=False, progress=False)
