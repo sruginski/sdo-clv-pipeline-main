ac396104c8476159a4ee7d93b5878fd56927c454
sruginski <ruginski_savannah@wheatoncollege.edu>
2025-06-30 16:37:39 -0400

    after Zoom
    Former-commit-id: 6fb6fa157ec41650ee369f360ab873ce451e5525

--- sdo_clv_pipeline/download_stuff.py
+++ sdo_clv_pipeline/download_stuff.py
@@ -1,4 +1,5 @@
 from sdo_clv_pipeline.sdo_download import *
 
-download_data(series="720", email="mlp95@psu.edu", outdir="/Users/srugins/sdo-clv-pipeline/data", start="2014/01/01", end="2014/01/02", 
+data_dir = os.path.abspath(os.path.join(os.getcwd(), "..", "data"))
+download_data(series="720", email="mlp95@psu.edu", outdir=data_dir, start="2014/01/01", end="2014/01/02", 
               sample=4, overwrite=False, progress=False)
