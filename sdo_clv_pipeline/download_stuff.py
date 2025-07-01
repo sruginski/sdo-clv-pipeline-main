3ef3849c89d61c6915fdd727ba1fa7d4924698de
Savannah Ruginski <srugins@nuha.cfa.harvard.edu>
2025-07-01 10:30:47 -0400

    debug data download
    Former-commit-id: 96ec9a680227026c02ed90a742ddba5c52306b05

--- sdo_clv_pipeline/download_stuff.py
+++ sdo_clv_pipeline/download_stuff.py
@@ -1,5 +1,6 @@
 from sdo_clv_pipeline.sdo_download import *
 
 data_dir = os.path.abspath(os.path.join(os.getcwd(), "..", "data"))
+print(data_dir)
 download_data(series="720", email="mlp95@psu.edu", outdir=data_dir, start="2014/01/01", end="2014/01/02", 
               sample=4, overwrite=False, progress=False)
