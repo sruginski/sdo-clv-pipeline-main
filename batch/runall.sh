beab680affc2bb59aa7be935cfe3e0aedd0ed64a
Michael Palumbo <palumbomi95@gmail.com>
2025-08-08 13:37:36 -0400

    ignore

--- batch/runall.sh
+++ batch/runall.sh
@@ -5,9 +5,9 @@
 ##SBATCH --mem-per-cpu=8192
 ##SBATCH --time=2:00:00
 #SBATCH --time=48:00:00
-#SBATCH --job-name=sdo_all
+#SBATCH --job-name=sdo_moat
 #SBATCH --chdir=/mnt/home/mpalumbo/work/savannah/sdo-clv-pipeline
-#SBATCH --output=/mnt/home/mpalumbo/work/logs/sdo_all.%j.out
+#SBATCH --output=/mnt/home/mpalumbo/work/logs/sdo_moat.%j.out
 
 echo "About to start: $SLURM_JOB_NAME"
 date
@@ -17,6 +17,6 @@ cd $SLURM_SUBMIT_DIR
 
 echo "About to start Python"
 # uv run scripts/run_pipe.py --globexp "2014*01*07*"
-uv run scripts/run_pipe.py --globexp "2014"
+uv run scripts/run_pipe.py
 echo "Python exited"
 date
